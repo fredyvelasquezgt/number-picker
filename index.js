377527ea896eef51442ee3ff29b00dc89fb996fd
Fredy Velasquez <macacoloco696@gmail.com>
2023-07-05 12:48:24 -0600

    numPicker

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
+function getNum() {
+	return  Math.floor(Math.random() * 10) + 1; 
+}
+
 class NumPicker extends React.Component {
 	render() {
 		return (
-			<h1>Your number is:</h1>
+			<h1>Your number is: {getNum()}</h1>
 		)
 	}
 }
