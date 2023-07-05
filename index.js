0b4fd8421411bef8dd497bda555c60f173a686fb
Fredy Velasquez <macacoloco696@gmail.com>
2023-07-05 15:41:23 -0600

    conditionals

--- index.js
+++ index.js
@@ -5,14 +5,20 @@ function getNum() {
 class NumPicker extends React.Component {
 	render() {
 		const num = getNum()
+		let msg;
+
+		if(num === 7) {
+			msg = <div>
+				<h2>You win!</h2>
+				<img src="https://giphy.com/gifs/ufc-290-ufc-ufc290-vozWNU9Gcs49G7UVV1"/>
+			</div>
+		} else {
+			msg = <p>You lose!</p>
+		}
 		return (
 			<div>
 				<h1>Your number is {num}</h1>
-				<p>{num === 7 ? 'Congrats': 'Unlucky!' } </p>
-				{
-					num === 7 && 
-					<img src="https://giphy.com/gifs/ufc-290-ufc-ufc290-vozWNU9Gcs49G7UVV1" />
-					}
+				{msg}
 			</div>
 		
 		)
